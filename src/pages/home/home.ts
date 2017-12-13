057fba34ca24c9f125c131674fd6c19b956c5402
Wesley Sartori <wbsartori@gmail.com>
2017-12-12 23:54:50 -0200

    Update home.ts
    Atualizando Busca

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { ListPage } from './../list/list';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ProdutoService } from '../../providers/produto-service/produto-service';
+import { FormControl } from '@angular/forms/src/model';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'page-home',
@@ -10,11 +12,18 @@ import { ProdutoService } from '../../providers/produto-service/produto-service'
 export class HomePage {
 
   public listaProduto = [];
+  searchQuery: string = '';
+  items: any;
   
   constructor (private produtoService: ProdutoService,
                public navCtrl: NavController,
                public navParams: NavParams) {
     this.getProdutos();
+     this.initializeItems();  
+  }
+  
+   initializeItems() {
+    this.items = this.listaProduto;
   }
 
   getProdutos() {
@@ -25,4 +34,28 @@ export class HomePage {
   goToEdit(produto){
    this.navCtrl.push(ListPage, {produto: produto})
   }
+   getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.descricao.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  } 
+  
+    /**
+ * Fitragem dos dados
+ * @param searchTerm 
+ */
+  filterItems(searchTerm){
+           return this.items.descricao.filter((item) => {
+               return item.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+           });  
+    }
 }
